460635f4832312b0d428be7e58d844b317cd3498
hanzch84 <73715848+hanzch84@users.noreply.github.com>
2024-07-12 17:28:46 +0000

    prototype

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,20 +32,48 @@ else:
 
     st.title("Text-to-Speech with OpenAI")
 
-    text_input = "\n" * 30
+    if 'input_text' not in st.session_state:
+        st.session_state.input_text = """1. 다음을 듣고, 남자가 하는 말의 목적으로 가장 적절한 것을 고르시오.
+                                M: Hello, Lockwood High School students. This is your school librarian,
+                                Mr. Wilkins. I’m sure you’re aware that our school library is hosting a
+                                bookmark design competition. I encourage students of all grades to
+                                participate in the competition. The winning designs will be made into
+                                bookmarks, which will be distributed to library visitors. We’re also giving
+                                out a variety of other prizes. So don’t let this great opportunity slip away.
+                                Since the registration period for the bookmark design competition ends this
+                                Friday, make sure you visit our school library to submit your application.
+                                Come and participate to display your creativity and talents.
+
+                                2. 대화를 듣고, 여자의 의견으로 가장 적절한 것을 고르시오.
+                                M: Honey, do you want some apples with breakfast?
+                                W: Sounds great. Can you save the apple peels for me?
+                                M: Why? What do you want them for?
+                                W: I’m going to use them to make a face pack. Apple peels are effective for
+                                improving skin condition.
+                                M: Where did you hear about that?
+                                W: I recently read an article about their benefits for our skin.
+                                M: Interesting. What’s in them?
+                                W: It said apple peels are rich in vitamins and minerals, so they moisturize our
+                                skin and enhance skin glow.
+                                M: That’s good to know.
+                                W: Also, they remove oil from our skin and have a cooling effect.
+                                M: Wow! Then I shouldn’t throw them away.
+                                W: Right. Apple peels can help improve our skin condition.
+                                M: I see. I’ll save them for you."""
+
 
     col_kr_voice, col_fe_voice, col_ma_voice, col_interval = st.columns([3, 3, 3, 3])
-    ko_option = col_kr_voice.radio("Select a Korean voice.", ['alloy', 'echo', 'fable', 'nova', 'onyx', 'shimmer'], key="korean_option")
+    ko_option = col_kr_voice.radio("Select a Korean voice.", ['alloy', 'echo', 'fable', 'nova', 'onyx', 'shimmer'], key="korean_option", index=2)
     female_voice = col_fe_voice.radio("Select a female voice.", ['alloy', 'fable', 'nova', 'shimmer'], key="female_option")
     male_voice = col_ma_voice.radio("Select a male voice.", ['echo', 'onyx'], key="male_option")
-    interline = col_interval.number_input("대사 간 간격(ms)", min_value=30, max_value=2000)
-    internum = col_interval.number_input("문제 간 간격(ms)", min_value=30, max_value=2000)
+    interline = col_interval.number_input("대사 간 간격(ms)", value=300, min_value=30, max_value=3000, key="interline")
+    internum = col_interval.number_input("문제 간 간격(ms)", value=300, min_value=30, max_value=3000, key="internum")
 
     col_btn1, col_btn2 = st.columns(2)
     st.write = female_voice
 
     col_lang, col_name, col_line = st.columns([1, 2, 8])
-    if col_btn1.button("Preprocessing",disabled=True):
+    if col_btn1.button("Preprocessing"):
         result = ''
         for line in st.session_state.input_text.split("\n"):
             line = line.lstrip()
@@ -61,9 +89,12 @@ else:
             col_lang.markdown(which_eng_kor(line))  # 언어 표시(필요시 활성화)
             col_name.markdown(name.strip())
             col_line.markdown(talk.strip())
+
     success_message = st.empty()
-    warning_message = st.empty()
+    en_warning_message = st.empty()
     audio_placeholder = st.empty()
+    kr_warning_message = st.empty()
+    text_input = st.text_area("Enter the text you want to convert to speech", st.session_state.input_text, key="input_area", height=st.session_state.input_text.count('\n') * 24)
 
     if col_btn2.button("Convert to Speech"):
         try:
@@ -102,45 +133,24 @@ else:
                     for chunk in response.iter_bytes():
                         tts.extend(chunk)
 
-                    tts.extend(b'\x00' * (interline * 16000 // 1000))  # Add interline interval
+                    tts.extend(b'\x00' * (st.session_state.interline * 16000 // 1000))  # Add interline interval
 
             with open(speech_file_path, 'wb') as audio_file:
                 audio_file.write(tts)
 
-            success_message.success("Speech conversion successful!")
-            warning_message.warning("Disclosure: The voice you are hearing is AI-generated and not a human voice.\n고지 사항: 이 목소리는 인공지능(AI)으로 생성된 것이며, 실제 사람의 목소리가 아닙니다.")
-            audio_placeholder.audio(str(speech_file_path))
+            st.session_state.speech_file_path = str(speech_file_path)
+            st.session_state.success_message = "Speech conversion successful!"
+            st.session_state.en_warning_message = "Disclosure: The voice you are hearing is AI-generated and not a human voice."
+            st.session_state.kr_warning_message = "고지 사항: 이 목소리는 인공지능(AI)으로 생성된 것이며, 실제 사람의 목소리가 아닙니다."
 
         except Exception as e:
-            success_message.error(f"An error occurred: {e}")
+            st.session_state.success_message = f"An error occurred: {e}"
+    
 
-    if 'input_text' not in st.session_state:
-        st.session_state.input_text = """1. 다음을 듣고, 남자가 하는 말의 목적으로 가장 적절한 것을 고르시오.
-                                M: Hello, Lockwood High School students. This is your school librarian,
-                                Mr. Wilkins. I’m sure you’re aware that our school library is hosting a
-                                bookmark design competition. I encourage students of all grades to
-                                participate in the competition. The winning designs will be made into
-                                bookmarks, which will be distributed to library visitors. We’re also giving
-                                out a variety of other prizes. So don’t let this great opportunity slip away.
-                                Since the registration period for the bookmark design competition ends this
-                                Friday, make sure you visit our school library to submit your application.
-                                Come and participate to display your creativity and talents.
 
-                                2. 대화를 듣고, 여자의 의견으로 가장 적절한 것을 고르시오.
-                                M: Honey, do you want some apples with breakfast?
-                                W: Sounds great. Can you save the apple peels for me?
-                                M: Why? What do you want them for?
-                                W: I’m going to use them to make a face pack. Apple peels are effective for
-                                improving skin condition.
-                                M: Where did you hear about that?
-                                W: I recently read an article about their benefits for our skin.
-                                M: Interesting. What’s in them?
-                                W: It said apple peels are rich in vitamins and minerals, so they moisturize our
-                                skin and enhance skin glow.
-                                M: That’s good to know.
-                                W: Also, they remove oil from our skin and have a cooling effect.
-                                M: Wow! Then I shouldn’t throw them away.
-                                W: Right. Apple peels can help improve our skin condition.
-                                M: I see. I’ll save them for you."""
-
-    text_input = st.text_area("Enter the text you want to convert to speech", st.session_state.input_text, key="input_area", height=st.session_state.input_text.count('\n') * 24)
+    if 'speech_file_path' in st.session_state:
+        success_message.success(st.session_state.success_message)
+        en_warning_message.warning(st.session_state.en_warning_message)
+        kr_warning_message.warning(st.session_state.kr_warning_message)
+        audio_placeholder.audio(st.session_state.speech_file_path)
+    
